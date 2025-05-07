f41e9c3424bbf909d6dc7f5da18128d6a4b7fd71
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-05-07 16:21:19 +0000

    Following along with lesson for generators

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_07_161809) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_07_162012) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "boards", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.date "expires_on"
+    t.integer "board_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
